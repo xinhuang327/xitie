0ed690f732aeba01661d44386d2eabdfd4be3bdd
Adrian Huang <xinhuang327@gmail.com>
2017-05-19 17:58:56 +0800

    1st

---
+++ xitie.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	router := gin.Default()
+	router.Static("/", "./wwwroot")
+
+	// Listen and serve on 0.0.0.0:8080
+	router.Run(":8080")
+}
